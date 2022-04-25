945a234b6252b7ee89bc5dd19aca5ee24111ba9a
karajovanovicana <karajovanovic.ana@gmail.com>
2022-04-25 18:33:28 +0200

    commit

--- frontend/src/custom-axios/axios.js
+++ frontend/src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9091/api',
+    baseURL: 'https://library-lab-emt.herokuapp.com/api',
     headers: {
         'Access-Control-Allow-Origin' : '*',
         // 'Authorization': localStorage.getItem("JWT")
